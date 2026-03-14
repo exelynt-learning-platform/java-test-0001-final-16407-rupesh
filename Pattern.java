4297f7e5256b32025845c5bff88defd1392f2155
rupesshhh-coder <176688492+rupesshhh-coder@users.noreply.github.com>
2026-03-14 18:27:08 +0530

    Create Pattern.java
    JAVA TEST 0001 Pattern Solution

---
+++ Pattern.java
@@ -0,0 +1,40 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+
+            for(int k=1;k<=2*(i-1)-1;k++){
+                System.out.print(" ");
+            }
+
+            if(i>1){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n-1;i>=1;i--){
+            for(int j=n;j>i;j--){
+                System.out.print(" ");
+            }
+
+            System.out.print("*");
+
+            for(int k=1;k<=2*(i-1)-1;k++){
+                System.out.print(" ");
+            }
+
+            if(i>1){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
